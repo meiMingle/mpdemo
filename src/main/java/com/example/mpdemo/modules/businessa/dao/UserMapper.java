7cc384a95f53c7a896f136f07d840625c2026b42
TomXin <766781886@qq.com>
2022-08-11 02:33:36 +0800

    Initial commit

---
+++ src/main/java/com/example/mpdemo/modules/businessa/dao/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.mpdemo.modules.businessa.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mpdemo.modules.businessa.domain.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/example/mpdemo/MpdemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.mpdemo;
+
+import com.example.mpdemo.modules.businessa.dao.UserMapper;
+import com.example.mpdemo.modules.businessa.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+@SpringBootTest
+class MpdemoApplicationTests {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    void contextLoads() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+
+}
