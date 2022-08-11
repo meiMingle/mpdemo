a6471aa2b8db9ed6e8315339d7dc559890243075
TomXin <766781886@qq.com>
2022-08-11 09:39:53 +0800

    update

--- src/main/java/com/example/mpdemo/MpdemoApplication.java
+++ src/main/java/com/example/mpdemo/MpdemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.mpdemo.**.dao")
+//@MapperScan("com.example.mpdemo.**.dao")
 public class MpdemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/mpdemo/modules/businessa/dao/UserMapper.java
+++ src/main/java/com/example/mpdemo/modules/businessa/dao/UserMapper.java
@@ -2,7 +2,9 @@ package com.example.mpdemo.modules.businessa.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mpdemo.modules.businessa.domain.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
