cb2c01b7e33ef92671acd6a63e03b7971a2e1404
TomXin <766781886@qq.com>
2022-09-02 23:49:27 +0800

    update test

--- src/test/java/com/example/mpdemo/MybatisPlusFastJUnit5Test.java
+++ src/test/java/com/example/mpdemo/MybatisPlusFastJUnit5Test.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
 
 @MybatisPlusTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ImportAutoConfiguration(value = {EmployeesServiceImpl.class})
+//@ImportAutoConfiguration(value = {EmployeesServiceImpl.class})
+@ContextConfiguration(classes = {EmployeesServiceImpl.class})
 @Rollback(value = true)//单元测试执行完,默认回滚事务，false不会回滚
 class MybatisPlusFastJUnit5Test {
     Logger logger = LoggerFactory.getLogger(getClass());
@@ -23,18 +25,19 @@ class MybatisPlusFastJUnit5Test {
     @Autowired
     EmployeesService employeesService;
 
-//TODO
+    //TODO
     @Test
     void baseServiceQuery() {
         logger.info("----- service count method test ------");
         long count = employeesService.count();
-        logger.info("总条数:{}",count);
+        logger.info("总条数:{}", count);
     }
-//TODO
+
+    //TODO
     @Test
     void baseMapperQuery() {
         logger.info("----- mapper count method test ------");
         long count = employeesMapper.selectCount(null);
-        logger.info("总条数:{}",count);
+        logger.info("总条数:{}", count);
     }
 }
