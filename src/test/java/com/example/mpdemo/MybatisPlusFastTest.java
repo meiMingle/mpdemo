962eef59fd272598cff96670276bd7928c44f26e
TomXin <766781886@qq.com>
2023-12-15 17:14:08 +0800

    更新

--- src/test/java/com/example/mpdemo/MybatisPlusFastTest.java
+++ src/test/java/com/example/mpdemo/MybatisPlusFastTest.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
 import com.example.mpdemo.modules.businessa.mapper.EmployeesMapper;
 import com.example.mpdemo.modules.businessa.service.EmployeesService;
 import com.example.mpdemo.modules.businessa.service.impl.EmployeesServiceImpl;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +16,7 @@ import org.springframework.test.annotation.Rollback;
 
 @MybatisPlusTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ImportAutoConfiguration(value = {EmployeesServiceImpl.class})
+@ImportAutoConfiguration(value = {PageHelperAutoConfiguration.class,EmployeesServiceImpl.class})
 @Rollback(value = true)//单元测试执行完,默认回滚事务，false不会回滚
 class MybatisPlusFastTest {
     Logger logger = LoggerFactory.getLogger(getClass());
@@ -36,5 +38,12 @@ class MybatisPlusFastTest {
         logger.info("----- mapper count method test ------");
         long count = employeesMapper.selectCount(null);
         logger.info("总条数:{}",count);
+    }
+
+    @Test
+    void pagehelpr() {
+        System.out.println(("----- pagehelper method test ------"));
+        long count = PageHelper.startPage(1, 2).doCount(() -> employeesMapper.selectList(null));
+        System.out.println(count);
     }
 }
